36820a8239402659a6d4a7763b0237aa0ceef2f4
TheTheMarcos <thethemarcos@gmail.com>
2024-04-17 18:51:21 +0200

    Eliminamos la funcion Greeting

--- app/src/main/java/com/example/jetmastuto/MainActivity.kt
+++ app/src/main/java/com/example/jetmastuto/MainActivity.kt
@@ -22,25 +22,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BirthdayCardPreview()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     JetMasTutoTheme {
-        Greeting("Marcos")
     }
 }
--- app/src/main/java/com/example/jetmastuto/ui/README.md
+++ app/src/main/java/com/example/jetmastuto/ui/README.md
@@ -1,3 +1,4 @@
 # Tutorial
 Nos vamos al main y modificamos GreetingPreview a BirthdayCardPreview()
 Cambiamos en la funcion BirthdayCardPreview() Android por Marcos
+Eliminamos Greeting
