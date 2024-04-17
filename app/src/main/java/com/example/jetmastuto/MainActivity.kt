788a421516fddfa9e767ad686215f5c066286868
TheTheMarcos <thethemarcos@gmail.com>
2024-04-17 18:48:32 +0200

    Cambiamos Android por Marcos

--- app/src/main/java/com/example/jetmastuto/MainActivity.kt
+++ app/src/main/java/com/example/jetmastuto/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     JetMasTutoTheme {
-        Greeting("Android")
+        Greeting("Marcos")
     }
 }
--- app/src/main/java/com/example/jetmastuto/ui/README.md
+++ app/src/main/java/com/example/jetmastuto/ui/README.md
@@ -1,2 +1,3 @@
 # Tutorial
 Nos vamos al main y modificamos GreetingPreview a BirthdayCardPreview()
+Cambiamos en la funcion BirthdayCardPreview() Android por Marcos
